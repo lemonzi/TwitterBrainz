7417c3c196003aafc1389c1c1a6482aa368f7c15
Àngel Farguell Caus <fcangel92@hotmail.com>
2015-02-09 19:13:52 +0100

    Filters end please!!

--- keywords.js
+++ keywords.js
@@ -22,17 +22,10 @@ module.exports = [
   },{
     query: "MakeAVoice com tunein",
     filters: [
-<<<<<<< HEAD
-    /(?:Radio: Now Playing "?)([^"]*)(?: - )([^"]*)(?:"?\n)/i,
-    /(?:Radio is now playing )(.*)(?: - )(.*)(?:\n)/i,
-    /(?:Playing: )(.*)(?: - )(.*)(?:\n)/i,
-    /(?:Now Playing )(.*)(?: - )(.*)(?: on )/i
-=======
       named(/(?:Radio: Now Playing "?)(:<song>[^"]*)(?: - )(:<artist>[^"]*)(?:"?\n)/i),
       named(/(Radio is now playing )(:<song>.*)(?: - )(:<artist>.*)(?:\n)/i),
       named(/(?:Playing: )(:<song>.*)(?: - )(:<artist>.*)(?:\n)/i),
       named(/(?:Now Playing )(:<song>.*)(?: - )(:<artist>.*)(?: on )/i)
->>>>>>> Filters
     ]
   },{
     query: "#EverythingThatRocks",
@@ -76,12 +69,6 @@ module.exports = [
       named(/(?:listening to )(:<song>.*)(?: by )(:<artist>[^#]*\S)(?: on @Grooveshark)/i)
     ]
   },{
-    query: "#SoundCloud",
-    filters: [
-      named(/(?:Listen to |A new favorite: )(:<song>.*?)(?: by )(:<artist>[^#]*\S)(?: #np on #SoundCloud| http.* on #SoundCloud| via #SoundCloud)/i),
-      named(/(?:Listen to |A new favorite: )(:<song>.*?)(?: by )(:<artist>[^#]*\S)(?: on #SoundCloud| http)/i)
-    ]
-  },{
     query: "itunes apple com album",
     filters: [
       named(/(?:#nowplaying )(:<song>.*)(?: by | - | – )(:<artist>[^#]*\S)(?: http)(?:.*)(?:itunes)/i),
@@ -89,71 +76,6 @@ module.exports = [
       named(/(?:.*iTunes(?:[^a-z]+\s|】))(:<song>\S.*) \W (:<artist>[\w ]*)(?:.*\s+.*http\S*)/i),
       named(/(:<song>\w[\wàèìòùáéíóúäëïöü'!, ]*[\wàèìòùáéíóúäëïöü'!,])(?:\s*(?:-(?: Single by)?)\s*)(:<artist>[\wàèìòùáéíóúäëïöü'!, ]*[\wàèìòùáéíóúäëïöü'!,])(?: from)(?:[\s+|.])(?:.*)/i),
       named(/(:<song>[\wàèìòùáéíóúäëïöü'!, ]*[\wàèìòùáéíóúäëïöü'!,])(?:\s*(?:-(?: Single by)?)\s*)(:<artist>[\wàèìòùáéíóúäëïöü'!, ]*[\wàèìòùáéíóúäëïöü'!,])(?:[\s+|.])(?:.*)/i)
-    ]
-  },{
-    query: "#np",
-    filters: [
-      named(/(?:#np New Video: )(:<song>.*)(?: by | - | – )(:<artist>[^#]*\S)(?: #)/i),
-      named(/(?:#np @.*: \')(:<song>.*)(?:\' by )(:<artist>.*)(?: - http)/i),
-      named(/(?:#np Now playing | #np #nowplaying)(:<song>.*)(?: by | - | – )(:<artist>[^#]*)(?:\s.*http)/i),
-      named(/(?:#np : )(:<song>.*)(?: - by )(:<artist>.*?)(?: #Deezer http)/i),
-      named(/(?:#np @?)(:<song>.*)(?: by #?| - #?| – #?)(:<artist>.*?\S)(?:\s+?-? on)/i),
-      named(/(?:#np @?)(:<song>.*)(?: by #?| - #?| – #?)(:<artist>.*?)(?: *[:@]? http| \| )/i)
-    ]
-  },{
-    query: "#Now Playing",
-    filters: [
-      named(/(?:#Now Playing on .* Radio: )(:<song>.*)(?: by | - | – )([^#]*\S)(?: http)/i),
-      named(/(?:#Now Playing )(:<artist>(?:\S-\S|[^-])*)(?:\s+-)?(?:\s+-\s+)(:<song>[\w\s-'!àèìòùáéíóúäëïöü.]*\w)(?: on WIAG.*)(?:http\S*)/i),
-      named(/(?:#Now Playing )(:<artist>(?:\S-\S|[^-])*)(?:\s+-)?(?:\s+-\s+)(:<song>[\w\s-'!àèìòùáéíóúäëïöü.]*\w)(?:.*\s+.*http\S*)/i),
-      named(/(?:#Now Playing )(:<artist>(?:\S-\S|[^-])*)(?:\s+-)?(?:\s+-\s+)(:<song>[\w\s]*\w)(?:.*\s+.*http\S*)/i)
-    ]
-  },{
-    query: "#nowplaying",
-    filters: [
-      named(/(?:#NowPlaying )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)( - listen at)/i),
-      named(/(?:#nowplaying on.*: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?:\s+~ listen now at)/i),
-      named(/(?:#nowplaying @ http[\S]+ )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: tune in now:)/i),
-      named(/(?:")(:<song>.*)(?:" track from ".*" album by )(:<artist>[^#]*\S)(?:#nowplaying)/i),
-      named(/(?:Listening to - )(:<song>.*)(?: ~~ )(:<artist>[^#]*\S)(?: #nowplaying)/i),
-      named(/(?:=&gt; )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #nowplaying)/i),
-      named(/(?:#NowPlaying - @)(:<song>.*)(?: by #| - #| – #)(:<artist>[^#]*\S)(?: : http)/i),
-      named(/(?:#NowPlaying ")(:<song>.*)(?:" by |" - |" – )(:<artist>[^#]*\S)(?: on @| on #| on http| on album)/i),
-      named(/(?:#NowPlaying ")(:<song>.*)(?:" by |" - |" – )(:<artist>[^#]*\S)(?: on)/i),
-      named(/(?:#NowPlaying #Music from )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http)/i),
-      named(/(?:#NowPlaying )(:<song>.*)(?: - | – )(?:Tribute to )(:<artist>[^#]*\S)(?:\n)/i),
-      named(/(?:Recomendación de viernes, )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: on http)/i),
-      named(/(?:listening to ')(:<song>.*)(?:' by ')(:<artist>[^#]*\S)(?:\s+'.)/i),
-      named(/(?:listening to )(:<song>.*)(?: by )(:<artist>[^#]*\S)(?: on @Grooveshark| on ReMoCon| on \(| on Real)/i),
-      named(/(?:listening to )(:<song>.*)(?: by )(:<artist>[^#]*\S)(?: http.*#nowplaying)/i),
-      named(/(?:listening to )(:<song>.*)(?: by )(:<artist>[^#]*\S)(?: #nowplaying| #)/i),
-      named(/(?:RT @.*: #NowPlaying )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: on @Grooveshark| on ReMoCon| on \(| is #nowplaying| \| http)/i),
-      named(/(?:RT @.*: #NowPlaying )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http)/i),
-      named(/(?:RT @.*: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: on @Grooveshark| on ReMoCon| on \(| is #nowplaying| \| http)/i),
-      named(/(?:RT @.*: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http)/i),
-      named(/(?:[\u266A-\u266F]\s)(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: [\u266A-\u266F])(?:.*\s+.*http\S*)/i),
-      named(/(?:♫\s*#NowPlaying )(:<song>\S.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: on #| on http)/i),
-      named(/(?:♫ |♫|♪ |♪)(:<song>\S.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: on #| on http)/i),
-      named(/(?:♫ |♫|♪ |♪)(:<song>\S.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http\S*)/i),
-      named(/(?:#NowPlaying :: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: on #| on http| on Music)/i),
-      named(/(?:#NowPlaying :: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #| http)/i),
-      named(/(?:#NowPlaying\s*: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?:\s::)(?:.*\s+.*http\S*)/i),
-      named(/(?:#NowPlaying\s*: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: \| )/i),
-      named(/(?:#NowPlaying\s*: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #.*http| http.*#)/i),
-      named(/(?:#NowPlaying\s*: )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #| http)/i),
-      named(/(?:#NowPlaying )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?:\s::)(?:.*\s+.*http\S*)/i),
-      named(/(?:#NowPlaying )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: solo por http)/i),
-      named(/(?:#NowPlaying )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: on #| on http| on Music| ~Listen Live At~| on .* Radio| from| EN http)/i),
-      named(/(?:#NowPlaying )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #| http)/i),
-      named(/(?:NowPlaying \/ )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http| #)/i),
-      named(/(?:NowPlaying |Now Playing : )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http.*#nowplaying)/i),
-      named(/(?:NowPlaying |Now Playing : )(:<song>.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #nowplaying)/i),
-      named(/(:<song>\S.*)(?: by )(:<artist>[^#]*\S)(?: on @Grooveshark| on ReMoCon| on \(| is #nowplaying| #nowplaying on| \: #nowplaying on)/i),
-      named(/(:<song>\S.*)(?: - )(:<artist>[^#]*\S)(?: \| http)/i),
-      named(/(:<song>\S.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http.*#nowplaying)/i),
-      named(/(:<song>\S.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #nowplaying.*http)/i),
-      named(/(:<song>\S.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: #nowplaying)/i),
-      named(/(:<song>\S.*)(?: by | - | – | ♪ by )(:<artist>[^#]*\S)(?: http)/i),
     ]
   }
 ];
